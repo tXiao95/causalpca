a9dba2d289fc5f9ec2795612ac9ac963110b8a7a
Thomas Hsiao <thsiao3@emory.edu>
2025-09-19 02:04:36 -0400

    Final simulation data tweaks for simulation experiment in ENAR paper

--- R/simulate_data.R
+++ R/simulate_data.R
@@ -1,15 +1,24 @@
 simulate_data <- function(n,
                           p = 20, 
                           q = 10,
-                          rho = 0.7) {
+                          rho = 0.8,
+                          h_Z_coef = 0.1,
+                          g_C_coef = 5,
+                          interaction_coef = 3) {
   # Confounders [C] ---------------------------------------------------------
   Sigma_C <- toeplitz(c(1, .5, rep(0, q-2)))
   C       <- MASS::mvrnorm(n = n, mu = rep(0, q), Sigma = Sigma_C)
 
+  # Inflate variance in X's that don't feed into beta to distract PCA 
+  idx_beta <- c(1:2, 17:20)
+  scale_vec <- rep(1, p)
+  scale_vec[-idx_beta] <- 3
+  D <- diag(scale_vec, p, p)
+  
   # Exposures [X | C] -------------------------------------------------------
-  Sigma_X <- toeplitz(rho^(0:((p)-1)))
-  Theta   <- matrix(1 / (1:q)^2, nrow = q, ncol = p)
-  X_mean  <- pnorm(C %*% Theta) + 0.75*rnorm(n) - 0.5
+  Sigma_X <- D %*% toeplitz(rho^(0:((p)-1))) %*% D
+  Theta   <- matrix(1:p / (1:q)^2, nrow = q, ncol = p)
+  X_mean  <- pnorm(C %*% Theta) 
   
   X <- t(apply(X_mean, 1, function(mu_i)
     MASS::mvrnorm(1, mu = mu_i, Sigma = Sigma_X)
@@ -17,18 +26,20 @@ simulate_data <- function(n,
   
   # 2 dimensions, first direction is 4, second is 2. 
   beta <- matrix(c(rep(1 / sqrt(2), 2),rep(0,18), 
-                 c(rep(0,16), rep(1 / sqrt(4), 4)) ),nrow = p, ncol = d)
+                 c(rep(0,16), rep(1 / sqrt(4), 4)) ),nrow = p, ncol = 2)
   # the unique Projection matrix from R^p to R^d
   P_beta <- beta %*% solve(t(beta) %*% beta) %*% t(beta)
   
-  # --- Outcome ---
+  # Dimension reduction
   Z   <- X %*% beta                   # n x 2
-  g_C <- C %*% Theta[,1]  
-  h_Z <- Z[, 1] + 0.1*Z[,2]^2
   
+  # --- Outcome ---
+  g_C   <- sin(C %*% Theta[,1])  
+  h_Z   <- Z[,1] + Z[,2]^2 + Z[,1]*Z[,2]
   eps_Y <- rnorm(n)
   
-  Y     <- h_Z + g_C + 0.1* Z[,1]*C[,1] + eps_Y * (sqrt( 0.5 + pnorm(rowSums(C[,1:2])) )) 
+  # Y response model
+  Y     <- h_Z_coef * h_Z + g_C_coef * g_C + interaction_coef * (Z[,1]*C[,1]) + eps_Y * (sqrt( 0.5 + pnorm(rowSums(C[,1:2])) )) 
   
   list(C = C, X = X, Y = Y, beta = beta, P_beta= P_beta)
 }
